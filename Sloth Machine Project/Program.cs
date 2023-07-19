9f2b3890e7701a92d266df051d73bb101d7f098f
1nature <olanipekun1439@yahoo.com>
2023-07-19 13:15:25 +0100

    Add project files.

---
+++ Sloth Machine Project/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sloth_Machine_Project
+{
+    internal class Program
+    {
+        const int minRandNumber = 0;
+        const int maxRandNumber = 20;
+        const int numberOfRows = 3;
+        const int numberOfColumns = 3;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the slot machine game");
+
+            Random generator = new Random();
+            int[,] arrayNumbers = new int[numberOfRows, numberOfColumns];
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                for (int column = 0; column < numberOfColumns; column++)
+                {
+                    arrayNumbers[row, column] = generator.Next(minRandNumber, maxRandNumber);
+                    Console.Write(arrayNumbers[row, column]);
+                }
+            }
+            
+        }
+    }
+}
