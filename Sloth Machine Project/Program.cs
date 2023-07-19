182619368656e4836c0edc5c6eb2735989370929
1nature <olanipekun1439@yahoo.com>
2023-07-19 15:11:27 +0100

    My first slot machine attempt

--- Sloth Machine Project/Program.cs
+++ Sloth Machine Project/Program.cs
@@ -8,8 +8,9 @@
         const int numberOfColumns = 3;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the slot machine game");
+            Console.WriteLine("Welcome to the slot machine game\n");
 
+            int wager = 3;
             Random generator = new Random();
             int[,] arrayNumbers = new int[numberOfRows, numberOfColumns];
 
@@ -18,10 +19,46 @@
                 for (int column = 0; column < numberOfColumns; column++)
                 {
                     arrayNumbers[row, column] = generator.Next(minRandNumber, maxRandNumber);
-                    Console.Write(arrayNumbers[row, column]);
+                    //Console.Write(arrayNumbers[row, column]);
                 }
             }
-            
+            Console.Write(arrayNumbers[0, 0]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[0, 1]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[0, 2]);
+            Console.WriteLine();
+            Console.Write(arrayNumbers[1, 0]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[1, 1]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[1, 2]);
+            Console.WriteLine();
+            Console.Write(arrayNumbers[2, 0]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[2, 1]);
+            Console.Write(",");
+            Console.Write(arrayNumbers[2, 2]);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Wager for the Sloth Machine above is $3");
+            Console.WriteLine("Enter 1 to make a wager, 0 to quit");
+            int wagerDecision = int.Parse(Console.ReadLine());
+
+            if (arrayNumbers[1, 0] == arrayNumbers[1, 1] && arrayNumbers[1, 0] == arrayNumbers[1, 2])
+            {
+                Console.WriteLine($"Your wager produced: {arrayNumbers[1, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[1, 2]}");
+                Console.WriteLine("You win $1");
+                wager++;
+                Console.WriteLine($"Your wager has increased to: {wager}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Your wager produced: {arrayNumbers[1, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[1, 2]}");
+                Console.WriteLine("They did not match. You lose");
+            }
         }
     }
+    
 }
