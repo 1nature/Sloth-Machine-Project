e62324a9bffe8b087d0e61af6e6766c0aca90ef2
1nature <olanipekun1439@yahoo.com>
2023-07-26 17:38:42 +0100

    #5 resolved. Winning conditions added

--- Sloth Machine Project/Program.cs
+++ Sloth Machine Project/Program.cs
@@ -13,7 +13,13 @@
         {
             Console.WriteLine("Welcome to the slot machine game\n");
 
-            int wager = 3;
+            int wager = 3; //Dollar amount
+            int wagerLoss = 3; //Dollar amount
+            int numberOfAttempts = 3;
+            int remainingAttempts = 3;
+            int wagerDecision;
+            bool keepPlaying = true;
+
             int[,] arrayNumbers = new int[NUMBEROFROWS, NUMBEROFCOLUMNS];
 
             for (int row = 0; row < arrayNumbers.GetLength(0); row++)
@@ -29,22 +35,84 @@
 
             Console.WriteLine("Wager for the Sloth Machine above is $3");
             Console.WriteLine("Enter 1 to make a wager, 0 to quit");
-            int wagerDecision = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
-            if (arrayNumbers[1, 0] == arrayNumbers[1, 1] && arrayNumbers[1, 0] == arrayNumbers[1, 2])
+            while (keepPlaying)
             {
-                Console.WriteLine($"Your wager produced: {arrayNumbers[1, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[1, 2]}");
-                Console.WriteLine("You win $1");
-                wager++;
-                Console.WriteLine($"Your wager has increased to: {wager}");
+                for (int tries = 0; tries < numberOfAttempts; tries++)
+                {
+                    wagerDecision = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (wagerDecision == 0)
+                    {
+                        Console.WriteLine("You have decided to quit this game");
+                        break;
+                    }
+
+                    else if (wagerDecision == 1)
+                    {
+                        //for the horizontal combination
+                        if (arrayNumbers[0, 0] == arrayNumbers[0, 1] && arrayNumbers[0, 0] == arrayNumbers[0, 2])
+                        {
+                            Console.WriteLine($"Your wager produced: {arrayNumbers[0, 0]}, {arrayNumbers[0, 1]}, {arrayNumbers[0, 2]}");
+                            Console.WriteLine("You win $1");
+                            wager++;
+                            Console.WriteLine($"Your wager has increased to: ${wager}");
+                        }
+
+                        else if (arrayNumbers[1, 0] == arrayNumbers[1, 1] && arrayNumbers[1, 0] == arrayNumbers[1, 2])
+                        {
+                            Console.WriteLine($"Your wager produced: {arrayNumbers[1, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[1, 2]}");
+                            Console.WriteLine("You win $1");
+                            wager++;
+                            Console.WriteLine($"Your wager has increased to: ${wager}");
+                        }
+
+                        else if (arrayNumbers[2, 0] == arrayNumbers[2, 1] && arrayNumbers[2, 0] == arrayNumbers[2, 2])
+                        {
+                            Console.WriteLine($"Your wager produced: {arrayNumbers[2, 0]}, {arrayNumbers[2, 1]}, {arrayNumbers[2, 2]}");
+                            Console.WriteLine("You win $1");
+                            wager++;
+                            Console.WriteLine($"Your wager has increased to: ${wager}");
+                        }
+
+                        //for the diagonal combination
+                        else if (arrayNumbers[0, 0] == arrayNumbers[1, 1] && arrayNumbers[0, 0] == arrayNumbers[2, 2])
+                        {
+                            Console.WriteLine($"Your wager produced: {arrayNumbers[0, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[2, 2]}");
+                            Console.WriteLine("You win $1");
+                            wager++;
+                            Console.WriteLine($"Your wager has increased to: ${wager}");
+                        }
+
+                        else if (arrayNumbers[2, 0] == arrayNumbers[1, 1] && arrayNumbers[2, 0] == arrayNumbers[0, 2])
+                        {
+                            Console.WriteLine($"Your wager produced: {arrayNumbers[2, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[0, 2]}");
+                            Console.WriteLine("You win $1");
+                            wager++;
+                            Console.WriteLine($"Your wager has increased to: ${wager}");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Your attempt has not produced a winning combination. Please try again");
+                            wagerLoss--;
+                            Console.WriteLine($"Your wager has decreased to: ${wagerLoss}");
+                        }
+                        remainingAttempts--;
+                        Console.WriteLine($"Remaining attempts = {remainingAttempts}");
+                    }
+                }
+
+                if (remainingAttempts == wagerLoss || wager == 6)
+                {
+                    break;
+                }
             }
 
-            else
-            {
-                Console.WriteLine($"Your wager produced: {arrayNumbers[1, 0]}, {arrayNumbers[1, 1]}, {arrayNumbers[1, 2]}");
-                Console.WriteLine("They did not match. You lose");
-            }
+            Console.WriteLine("End of the game");
         }
     }
-    
+
 }
